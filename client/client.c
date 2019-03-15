4309d1568b06f887a68446af24615f5c7b6e6bd4
ETZhangSX <etzhangsx@qq.com>
2019-03-16 01:59:09 +0800

    fixed that cannot load the html

--- client/client.c
+++ client/client.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
 
-int main() {
-	
+#define BUF_SIZE 1024
+#define SMALL_BUF 100
+
+void* request_handler(void* arg);
+void send_data(FILE* fp, char* ct, char* file_name);
+char* content_type(char* file);
+void send_error(FILE* fp);
+void error_handling(char *message);
+
+int main(int argc, char *argv[]) {
+	int serv_sock, clnt_sock;
+	struct  sockaddr_in serv_adr, clnt_adr;
+    int clnt_adr_size;
+    char buf[BUF_SIZE];
+    pthread_t t_id;
+    if (argc!=2) {
+        printf("Usege : %s <port>\n", argv[0]);
+        exit(1)'
+    }
+    
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    memset(&serv_adr, 0, sizeof(serv_adr));
 }
