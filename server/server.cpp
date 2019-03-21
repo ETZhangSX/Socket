3b7d317cdeb86c17ac247239656267f2df17cae2
ETZhangSX <etzhangsx@qq.com>
2019-03-21 15:34:46 +0800

    Fixed some problem

--- server/server.cpp
+++ server/server.cpp
@@ -291,10 +291,10 @@ void sendJPG(int *sock, char *filename) {
     int client_sock = *sock;
     char buffer[buffer_size];
     FILE *fp;
-    // FILE *fw;
+    FILE *fw;
     
     char status[] = "HTTP/1.1 200 OK\r\n";
-    char header[] = "Server: A Simple Web Server\r\nContent-Type: image/jpeg\r\n\r\n";
+    char header[] = "Server: A Simple Web Server\r\nContent-Type: image\r\n\r\n";
     
     write(client_sock, status, strlen(status));
     write(client_sock, header, strlen(header));
@@ -307,19 +307,19 @@ void sendJPG(int *sock, char *filename) {
         return;
     }
     printf("Sending img\n");
-    // fw = fdopen(client_sock, "w");
+    fw = fdopen(client_sock, "w");
     fread(buffer, 1, sizeof(buffer), fp);
-    // while (!feof(fp)) {
-    //     fwrite(buffer, 1, sizeof(buffer), fw);
-    //     fread(buffer, 1, sizeof(buffer), fp);
-    // }
-    while(!feof(fp)) {
-        write(client_sock, buffer, strlen(buffer));
+    while (!feof(fp)) {
+        fwrite(buffer, 1, sizeof(buffer), fw);
         fread(buffer, 1, sizeof(buffer), fp);
     }
+    // while(!feof(fp)) {
+    //     write(client_sock, buffer, strlen(buffer));
+    //     fread(buffer, 1, sizeof(buffer), fp);
+    // }
 
     
-    // fclose(fw);
+    fclose(fw);
     fclose(fp);
     close(client_sock);
 }
